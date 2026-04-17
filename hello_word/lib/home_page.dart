e0270221f6e0a0385b00539655ee7f64adb3493c
Seu Nome Completo <rhayonne@gmail.com>
2026-04-17 15:22:22 +0200

    Single render alinhamento

--- hello_word/lib/home_page.dart
+++ hello_word/lib/home_page.dart
@@ -15,16 +15,17 @@ class HomePageStates extends State<HomePage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home  Page')),
-
-      body: Center(
-        child: GestureDetector(
-          child: Text('Contador: $counter', style: TextStyle(fontSize: 20)),
-          onTap: () {
-            setState(() {
-              counter++;
-            });
-          },
+      appBar: AppBar(
+        title: Text('Home  Page'),
+        backgroundColor: const Color.fromARGB(255, 255, 0, 0),
+      ),
+      body: Container(
+        height: 200,
+        width: 200,
+        color: Colors.black,
+        // quando se single render,deve se inserir o alinhamento para renderizar o quadrado dentro do outro se nao ele vai sobrepor um sobre o outro
+        child: Center(
+          child: Container(height: 50, width: 50, color: Colors.amber),
         ),
       ),
       floatingActionButton: FloatingActionButton(
