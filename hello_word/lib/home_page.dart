cc071b9380bf308b0e8e957328f65f23f153db0f
Seu Nome Completo <rhayonne@gmail.com>
2026-04-21 12:20:11 +0200

    Foto arredondada

--- hello_word/lib/home_page.dart
+++ hello_word/lib/home_page.dart
@@ -23,8 +23,12 @@ class HomePageStates extends State<HomePage> {
         child: Column(
           children: [
             UserAccountsDrawerHeader(
-              currentAccountPicture: Image.network(
-                'https://plus.unsplash.com/premium_vector-1719858611039-66c134efa74d?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+              //para colocar como oval, so precisamos usar o ClipOval, nesse caso usamos o ClipRRect para demonstrar que se pode usar essa widget em outros lugares também.
+              currentAccountPicture: ClipRRect(
+                borderRadius: BorderRadius.circular(40), //arredondar
+                child: Image.network(
+                  'https://plus.unsplash.com/premium_vector-1719858611039-66c134efa74d?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                ),
               ),
               accountName: Text('Rhay'),
               accountEmail: Text(emailLogin),
